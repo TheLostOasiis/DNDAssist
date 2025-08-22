b0ec22dbc714bd37b339a8f77162c1fbe9067410
TheLostOasiis <thelostoasiis@icloud.com>
2025-08-22 12:51:23 -0400

    Continued Building of character class, will add custom classes for each charcater class

--- src/java/character/character.java
+++ src/java/character/character.java
@@ -3,6 +3,9 @@ package java.character;
 class Character {
     String name;
     int health;
+    boolean deathSavingThrows = false;
+    int fails = 0;
+    int Succseed = 0;
 
     public Character (String name, int health){
         this.name = name;
@@ -13,10 +16,21 @@ class Character {
     public void takeDamage(int amount){
         health -= amount;
         System.out.println(name + "takes " + amount + " damage. Remaining HP: " + health);
+
+        if (health <= 0) {
+            System.out.println(name + " has been knocked unconscious, Death saving throws active");
+            deathSavingThrows = true;
+        }
     }
 
-    class Wizard extends Character {
-        int mana;
+    public void deathSavingThrows(int roll){
+        if (roll >= 10){
+            System.out.println("Your roll has succeeded, Adding one roll to your successes.");
+            Succseed ++;
+        }else {
+            System.out.println("Your roll has FAILED, Adding one roll to your FAILS.");
+            fails ++;
+        }
     }
 
 }
